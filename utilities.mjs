875a7bcc4298a270607f3a608fc5f0ec6990cf31
Alan Lam <s3436174@student.rmit.edu.au>
2023-01-15 10:25:47 +1100

    Update bulk function export

--- utilities.mjs
+++ utilities.mjs
@@ -1,13 +1,21 @@
 
 // export const checkEmail = email => console.log(email)
-export const checkEmail = email => email.match(/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+$/)
+const checkEmail = email => email.match(/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+$/)
 
-export const checkPhoneNumber = number => number.match(/^((\+61|0)?\s?[2|3|4|7|8])?\d{2}\s?\d{2}\s?\d\s?\d{3}$/)
+const checkPhoneNumber = number => number.match(/^((\+61|0)?\s?[2|3|4|7|8])?\d{2}\s?\d{2}\s?\d\s?\d{3}$/)
 
-export const checkEmpty = input => {
+const checkEmpty = input => {
     if (input === "" || input === undefined || input === null ) {
         return false;
     }
 
     return true;
+}
+
+const checkForCapital = char => char.match(/^[A-Z]$/)
+
+export {
+    checkEmail,
+    checkPhoneNumber,
+    checkForCapital
 }
